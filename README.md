89b0ef68f74d7955d764885d7edb7650847d5bd0
Eduardo Candia <eduardo.candia@enroutesystems.com>
2020-11-27 15:21:00 -0600

    Primer Commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # evaluacion_rg5
 Examen Primera Semana Rockstar 5G
+
+### Requisitos
+- Python 3
+- Docker
+
+### Instrucciones:
+- Clonar este repo
+- Crear imagen de docker
+```
+docker build -t "eva_rg5"
+```
+- Correr imagen en modo developer (salir al cerrar)
+```
+docker run -it -p 5000:5000 --rm --name "eva_rg5-volatil" eva_rg5
+```
+- Correr imagen en modo servicio
+```
+docker run -p 5000:5000 -d  --name "eva_rg5-serv" eva_rg5
+```
---
+++ src/web_flask.py
@@ -0,0 +1,18 @@
+import csv
+from flask import Flask, jsonify
+
+app = Flask(__name__, static_url_path="")
+
+
+# noinspection PyPackageRequirements
+@app.route('/', methods=['GET'])
+def jaison():
+    results = []
+    with open('data/employees.csv') as File:
+        reader = csv.DictReader(File)
+        for row in reader:
+            results.append(row)
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=6000)
